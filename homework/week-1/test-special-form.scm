c49b5911ffb0c467fd82fdd806a382992e6e0c9e
JVPH <gyrospizzeria@protonmail.com>
2024-01-16 10:35:10 -0500

    solution to question 6

---
+++ homework/week-1/test-special-form.scm
@@ -0,0 +1,14 @@
+; Both 'and' and 'or' are special forms, and we know because both of these functions would run an infinite loop otherwise
+
+
+
+
+(define (test-and-sf x y z)
+    (if (and (= x 1) (= y 0) (= z (test-or-sf x y z)))
+        #t
+        #f))
+
+(define (test-and-sf x y z)
+    (if (or (= x 1) (= y 0) (= z (test-or-sf x y z)))
+        #t
+        #f))
