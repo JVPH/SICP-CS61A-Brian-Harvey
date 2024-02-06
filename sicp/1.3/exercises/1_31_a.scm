cfb5b1be150f29680b9b09cf299c78d1fb9f7835
JVPH <gyrospizzeria@protonmail.com>
2024-02-05 19:13:46 -0500

    solution to 1.31(a)

--- sicp/1.3/exercises/1_31_a.scm
+++ sicp/1.3/exercises/1_31_a.scm
@@ -6,4 +6,10 @@
 (define (factorial n)
   (product (lambda (x) x) 2 (lambda (x) (+ x 1)) n))
 
-    
+(define (1+ x)
+  (+ x 1))
+
+(define (pi terms) (* 4 (product
+			 (lambda (x) (/ (* 2 (1+ (floor (/ x 2))))
+					(1+ (* 2 (ceiling (/ x 2))))))
+			 1 1+ terms)))
